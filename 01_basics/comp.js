5f6269ef44a536b66e7a3b3655a9750907a6de95
Devdatta Raorane <111450782+devdattaraorane@users.noreply.github.com>
2025-11-23 15:55:11 +0000

    message:"hello"

--- 01_basics/comp.js
+++ 01_basics/comp.js
@@ -1,3 +1,8 @@
 //console.log("2">2);
-console.log("2"==2)
-console.log("20"===20)
+//console.log("2"==2)
+//console.log("20"===20)
+console.log(undefined ==0);
+console.log(undefined>=0);
+
+console.log(null>=0);
+
