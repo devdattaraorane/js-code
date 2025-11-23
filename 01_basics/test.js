b7f5a882a9202620e6632f79c2360a499f21d0c9
Devdatta Raorane <111450782+devdattaraorane@users.noreply.github.com>
2025-11-23 13:24:45 +0000

    new

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,8 @@
-console.log("Devdatta")
-console.log("Hello World")
+const accountId=14556;
+let accountEmail="dev@gmail.com"
+var accountPassword="12e1"
+
+accountEmail="rao@gmail.com"
+accountCity="Airoli"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
