626e29334a9ea9d6f65b35f5a986e51d275494bd
Devdatta <devdattaraorane607@gmail.com>
2025-11-27 08:43:31 +0530

    message:"ghello"

---
+++ 01_basics/ext1.js
@@ -0,0 +1,7 @@
+var v = 9;
+function call() {
+    for (let i = 1; i < 7; i++) {
+        document.write("<h" + i + ">Hello</h" + i + ">");
+        document.write("<font size=" + i + ">Hello</font>");
+    }
+}
---
+++ 01_basics/practice.js
@@ -0,0 +1,10 @@
+// let a;        // hoisted
+// console.log(a); // undefined
+// a = 10;
+
+function go()
+{
+    document.writeln("hello");
+}
+
+go();
